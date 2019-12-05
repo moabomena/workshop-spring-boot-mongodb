7f0909be7a616e8d0873166cd1c0eb90dda24b04
Moab Omena <1moabomena@gmail.com>
2019-12-05 14:02:01 -0300

    operação de instanciação da base de dados

---
+++ src/main/java/com/moabomena/workshopmongodb/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.moabomena.workshopmongodb.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.moabomena.workshopmongodb.domain.User;
+import com.moabomena.workshopmongodb.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+}
