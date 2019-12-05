d468d73c79355109d41811310f6d1ffa9e6076ed
Moab Omena <1moabomena@gmail.com>
2019-12-05 14:19:47 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/moabomena/workshopmongodb/domain/User.java
+++ src/main/java/com/moabomena/workshopmongodb/domain/User.java
@@ -10,9 +10,9 @@ public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	public String id;
-	public String name;
-	public String email;
+	private String id;
+	private String name;
+	private String email;
 
 	public User() {
 	}
