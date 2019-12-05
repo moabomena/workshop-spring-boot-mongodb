eaeafd026074aea05409c1d128d83e05ed984e8b
Moab Omena <1moabomena@gmail.com>
2019-12-04 22:01:30 -0300

    Projeto criado

---
+++ src/main/java/com/moabomena/workshopmongodb/WorshopmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.moabomena.workshopmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorshopmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorshopmongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moabomena/workshopmongodb/WorshopmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moabomena.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorshopmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
