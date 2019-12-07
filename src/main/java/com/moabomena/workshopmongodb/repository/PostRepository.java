91f18c88ae71572f3364c12a45c246df638d3e5f
Moab Omena <1moabomena@gmail.com>
2019-12-07 11:52:27 -0300

    Consulta simples com @Query

--- src/main/java/com/moabomena/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/moabomena/workshopmongodb/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.moabomena.workshopmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.moabomena.workshopmongodb.domain.Post;
@@ -10,6 +11,9 @@ import com.moabomena.workshopmongodb.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post>findByTitleContainingIgnoreCase(String text);
 
 }
